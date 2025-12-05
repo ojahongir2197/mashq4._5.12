d229250af811d2abb3666ec60da9abeb11673c6e
Jahongir <ojahongir2197@gmail.com>
2025-12-05 15:20:02 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+# 1-misol
+for i in range(3):
+    print("$ " * 5)
+
+#2-misol
+for i in range(5):
+    print("0 " * i * 1)
+
+#3-misol
+for i in range(2, 5):
+    print("@ " * i)
+for i in range(3, 1, -1):
+    print("@ " * i)
+
+#4-misol
+for i in range(1, 5):
+    print(f"{i} " * i + "#")
